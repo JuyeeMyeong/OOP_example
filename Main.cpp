2683070740013b46baabceff177a427584735814
Juyee Myeong <juyee.ming@gmail.com>
2025-05-26 17:38:12 +0900

    메모리 해제 / Main에 Test 코드 추가

--- Main.cpp
+++ Main.cpp
@@ -1,10 +1,13 @@
 #include <cstdlib> // rand()
 #include <ctime>
+#include <iostream>
 
 #include "Zoo.h"
 #include "Dog.h"
 #include "Cat.h"
 #include "Cow.h"
+
+using namespace std;
 
 // 랜덤 동물을 생성하는 함수
 // - 0, 1, 2 중 하나의 난수를 생성하여 각각 Dog, Cat, Cow 객체 중 하나를 동적으로 생성합니다.
@@ -28,6 +31,26 @@ Animal* createRandomAnimal()
 
 int main()
 {
+    cout << "동물원에 어서오세요!" << endl;
+    Zoo zoo; // 동물원 생성성
+    
+    // 동물원에 동물 8마리를 랜덤으로 추가
+    for (int i = 0; i < 8; i++)
+    {
+        Animal* animal = createRandomAnimal(); // 동물을 랜덤하게 생성
+        zoo.addAnimal(animal); // 동물을 동물원에 넣음
+    }
+
+    zoo.performActions();
+
+/*
+    Dog* puppy = new Dog();
+    puppy->makeSound();
+    Cat* kitten = new Cat();
+    kitten->makeSound();
+    Cow* moo = new Cow();
+    moo->makeSound();
+*/
 
     return 0;
 }
--- Zoo.cpp
+++ Zoo.cpp
@@ -5,24 +5,29 @@ using namespace std;
 
 void Zoo::addAnimal(Animal* animal)
 {
-    if (CountAnimals >= 10) // 
+    if (CountAnimals >= 10) // 동물원에 동물들이 다 차있을 때때
     {
         cout << "동물원이 꽉 찼습니다!" << endl;
-        return;
+        return; // 그냥 return
     }
 
-    animals[CountAnimals++] = animal;
+    animals[CountAnimals++] = animal; // 동물을 배열에 추가함함
 }
 
 void Zoo::performActions()
 {
     for (int i = 0; i < CountAnimals; i++)
     {
-        animals[i]->makeSound();
+        animals[i]->makeSound(); // 동물원에 있는 동물들의 makeSound() 함수 불러옴옴
     }
 }
 
-Zoo::~Zoo()
+Zoo::~Zoo() // 동물 객체 메모리 해제
 {
+    for (int i = 0; i < CountAnimals; i++)
+    {
+        delete animals[i];
+        animals[i] = nullptr;
+    }
     cout << "Zoo가 소멸되었습니다." << endl;
 }
